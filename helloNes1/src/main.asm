2a12cad7f2fd024aa1e2a3f0037b343cbe2ff7bc
aik <aik.wk@mail.com>
2025-09-03 17:09:51 +0100

    wip : loading background

--- helloNes1/src/main.asm
+++ helloNes1/src/main.asm
@@ -14,10 +14,21 @@
 .segment "STARTUP"
 
 .segment "CHARS"
+  .org $0000
+  .incbin "tiles.chr"
+
+.segment "RODATA"
+
+palleteData: 
+  .incbin "palettes.pal"
+
+bgData:
+  .incbin "bg.nam"
+bgDataSize = * - bgData
 
 .segment "CODE"
 
-.proc nmi
+.proc nmi     ; game loop goes here
   bit $2002
   lda #0
   sta $2006
@@ -25,19 +36,40 @@
   rti
 .endproc
 
-.proc ResetPalettes
+.proc LoadPalettes
   bit $2002
-  lda #$3f
+  lda #$3f    ; set PPUADDR to $3f00 
   sta $2006
   lda #$00
   sta $2006
-  lda #$0F
-  ldx #$20
-@paletteLoadLoop:
+
+  ldx #0  ; loop 
+@LoadLoop:
+  lda palleteData, x
   sta $2007
-  dex
-  bne @paletteLoadLoop
+  inx
+  cpx #32 ; 4(sets) * 4(colors) * 2(bytes) = 32 times
+  bne @LoadLoop
+
   rts
+.endproc
+
+.proc LoadBackground
+  bit $2002
+  lda #$20    ; set PPUADDR to $2000 
+  sta $2006
+  lda #$00
+  sta $2006
+
+  ldx #0  ; loop 
+@LoadLoop:
+  lda bgData, x
+  sta $2007
+  inx
+  cpx #bgDataSize
+  bne @LoadLoop
+
+  rts 
 .endproc
 
 .proc reset
@@ -48,13 +80,15 @@
   ldx #$ff
   txs
   ldx #0
-  stx $2000
-  stx $2001
+  stx $2000 ; disable nmi
+  stx $2001 ; disable rendering
   stx $4010
   bit $2002
+
 @vblankWait1:
   bit $2002
   bpl @vblankWait1
+
 @clearMemory:
   lda #$00
   sta $0000, x
@@ -67,10 +101,13 @@
   sta $0700, x
   inx
   bne @clearMemory
+
 @vblankWait2:
   bit $2002
   bpl @vblankWait2
-  jsr ResetPalettes
+
+  jsr LoadPalettes
+  jsr LoadBackground
 main:
   jsr AppMain
   lda #%00001000
